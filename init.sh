56f815896edf0414c766cfbb2a53d4b13f0a54ed
MarriccZwei <marricc7@gmail.com>
2025-09-16 23:20:17 -0700

    g

--- init.sh
+++ init.sh
@@ -218,7 +218,7 @@ download_url() {
         else
             ZBASENAME=""
         fi
-        (timeout 300 curl -s -S -R -L -O -k "$ZBASENAME" "$URL" || (echo "retrying curl $URL" && rm -f "$BASENAME" && curl -R -L -O -k "$URL")) && echo "Downloaded $URL"
+        (timeout 300 /mingw64/bin/curl -s -S -R -L -O -k "$ZBASENAME" "$URL" || (echo "retrying curl $URL" && rm -f "$BASENAME" && /mingw64/bin/curl -R -L -O -k "$URL")) && echo "Downloaded $URL"
     fi
 }
 
